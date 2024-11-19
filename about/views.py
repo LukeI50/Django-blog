e72fec41e5dc6e6b2a582e0c4b4e0039a15f99e6
(Philip) Luke <180131099+LukeI50@users.noreply.github.com>
2024-11-19 18:44:14 +0000

    Add: collaboration form

--- about/views.py
+++ about/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from .models import About
+from .forms import CollaborateForm
 
 # Create your views here.
 
 def about_detail(request):
     about = About.objects.all().order_by("-updated_on").first()
-    return render(request, "about/about_page.html", {"about": about})
+
+    collaborate_form = CollaborateForm()
+
+    return render(
+        request,
+        "about/about_page.html",
+        {
+            "about": about,
+            "collaborate_form": collaborate_form,
+        },
+    )
+
+
     
